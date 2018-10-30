59f748d7b5edc2e9b8eb0c5d1dcee5f0e36784b3
Aditya Shinde <shinde.aditya386@gmail.com>
2018-10-29 22:19:18 -0400

    Finished data recorder

--- loganalyser/data_recorder/data_recorder.py
+++ loganalyser/data_recorder/data_recorder.py
@@ -1,7 +1,5 @@
 import logging
 import subprocess
-import argparse
-import time
 
 
 class Recorder(object):
@@ -16,28 +14,19 @@ class Recorder(object):
         self.logger.info("Initialised data recorder.")
         self.logger.debug(f"Data recorder command: {self.__command}")
 
-# def get_cmdline():
-#     """
-#         Parse command line arguments and return values for output file and
-#         file size.
-#     """
-#     parser = argparse.ArgumentParser()
-#     parser.add_argument("w", help="name of the output file.")
-#     parser.add_argument("t", help="time to run sysdig.")
-#     args = parser.parse_args()
-#     logging.info(f"Starting data gathering.")
-#     return args
-#
-#
-# def gather_data(output_file, run_time):
-#     """
-#         Start sysdig data capture.
-#     """
-#     logging.info(f"Starting to gather data in {output_file} for {run_time}sec")
-#     command = f'sysdig -p %evt.time,%proc.name,%proc.pid,%proc.pname,%proc.ppid,%evt.type'
-#     logging.debug(f"Running command: {command}")
-#     with open(output_file, 'w') as o_file:
-#         sysdig = subprocess.Popen(command.split(), stdout=o_file)
-#         time.sleep(int(run_time))
-#         sysdig.terminate()
-#     logging.info(f"Finished recording data. Closing {sysdig}")
+    def start_recording(self, log_file):
+        """
+            Starts sysdig and outputs the logs to the given file
+        """
+        self.__l_file = open(log_file, 'w')
+        self.sysdig = subprocess.Popen(self.__command.split(),
+                                       stdout=self.__l_file)
+        self.logger.info(f"Data recording started. Logging in {log_file}")
+
+    def stop_recording(self):
+        """
+            Terminates sysdig process.
+        """
+        self.__l_file.close()
+        self.sysdig.terminate()
+        self.logger.info(f"Data recording stopped.")
